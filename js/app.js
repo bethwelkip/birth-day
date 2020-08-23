60ceb7d02a2d545d74afb32ce8430bc42ffd49e4
Bethwelkip <bkiplimo@princeton.edu>
2020-08-23 07:21:07 -0400

    add event listener and validation

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+function validate(){
+
+}
+
+function day(day, month, year) {
+
+var male = [Kwasi,  Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
+var female = [Akosua,  Adwoa, Abenaa, Akua,  Yaa,  Afua, Ama]
+document.getElementsByName("result").innerHTML = year;
+}
+
+var day = document.getElementsByName('date');
+var month = document.getElementsByName('month');
+var year = document.getElementsByName('year');
+
+document.addEventListener("DOMLoaded", validate())
