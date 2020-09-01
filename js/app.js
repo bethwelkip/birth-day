2b4d8ab30bf41847d7f29b86fb72c0a1e8d753f4
Bethwelkip <bkiplimo@princeton.edu>
2020-09-01 03:41:38 -0400

    add a function to select day of week

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
-
-
+const males = [];
+const females = [];
+const gender = document.getElementById('gender').value();
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
-  alert("here");
 	validate();
 });
 
 function validate(){
   const day = document.getElementsByName('date').value();
-  var date = new Date(day);
-  const weekday = date.getDay();
-  alert( "Please provide your name!" );
-  alert(weekday);
+  var date = new Date(Date.parse(day));
+  const dateString = date.toDateString().split(" ");
+	var weekday = dateString[0];
+	if (dateString[3] > 2020 || dateString[3] < 1900){
+		document.innerHTML
+	}
+	else{
+		console.log(weekday);
+		akanName(weekday);
+	}
 
+}
+function dayOfWeek(weekday){
+	if weekday === 'Mon'{return 1;}
+	else if(weekday === 'Tue'){ return 2;}
+	else if(weekday === 'Wed'){return 3;}
+	else if (weekday === 'Thu'){return 4;}
+	else if(weekday === 'Fri'){return 5;}
+	else if (weekday=== 'Sat'){return 6;}
+	else{return 0;}
+}
+function akanName(weekday){
+	if (gender == 'male'){
+		document.getElementById("result").innerHTML = "You were born on a" + weekday + "and your Akan Name is" + males[dayOfWeek(weekday)] + "!"
+	}
+	else{
+	document.getElementById("result").innerHTML = "You were born on a" + weekday + "and your Akan Name is" + females[dayOfWeek(weekday)] + "!"
+	}
 }
